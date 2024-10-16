efe34870cf593641fe864aa47600dca7d25d8325
Sulaimon Omotosho <susudef2u@gmail.com>
2024-10-16 13:02:49 +0100

    Chore: Typescript fix for data

--- components/dashboard/Table.tsx
+++ components/dashboard/Table.tsx
@@ -8,41 +8,51 @@ import {
 import { Table, TableBody, TableHead, TableHeader, TableRow } from '../ui/table'
 import { Columns, Orders } from '@/lib/types'
 
-const TableComponent = ({
+type TableComponentProps<T> = {
+  columns: Columns[]
+  title: string
+  description: string
+  renderRow: (item: T) => React.ReactNode // T is the generic type for the row item
+  data: T[] // T[] is the type of the data array
+}
+
+const TableComponent = <T,>({
   title,
   description,
   columns,
   renderRow,
   data,
-}: {
-  columns: Columns[]
-  title: string
-  description: string
-  renderRow: (item: Orders) => React.ReactNode
-  data: Orders[]
-}) => {
-  return (
-    <Card className='min-h-[calc(100vh-56px)] max-h-[calc(100vh-56px)] overflow-hidden'>
-      <CardHeader className='px-7'>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{description}</CardDescription>
-      </CardHeader>
-      <CardContent className='max-h-[calc(100vh-56px)] overflow-scroll remove-scrollbar pb-24'>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              {columns.map((col) => (
-                <TableHead key={col.accessor} className={col.className}>
-                  {col.header}
-                </TableHead>
-              ))}
-            </TableRow>
-          </TableHeader>
-          <TableBody>{data.map((item) => renderRow(item))}</TableBody>
-        </Table>
-      </CardContent>
-    </Card>
-  )
-}
+}: TableComponentProps<T>) =>
+  // {
+  //   columns: Columns[]
+  //   title: string
+  //   description: string
+  //   renderRow: (item: Orders) => React.ReactNode
+  //   data: Orders[]
+  // }
+  {
+    return (
+      <Card className='min-h-[calc(100vh-56px)] max-h-[calc(100vh-56px)] overflow-hidden'>
+        <CardHeader className='px-7'>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
+        <CardContent className='max-h-[calc(100vh-56px)] overflow-scroll remove-scrollbar pb-24'>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                {columns.map((col) => (
+                  <TableHead key={col.accessor} className={col.className}>
+                    {col.header}
+                  </TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody>{data.map((item) => renderRow(item))}</TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    )
+  }
 
 export default TableComponent
